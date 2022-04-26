d58f3c5bc39e08dee77952acbc6b2d909d183f5d
Prasadkpd <2019cs078@stu.ucsc.lk>
2022-04-26 13:00:15 +0530

    adding routing

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import config from 'config';
 import log from './logger';
 import connect from "./db/connect";
-import routes from "../routes";
+import routes from "./routes";
 
 const port = config.get("port") as number;
 const host = config.get("host") as string;
