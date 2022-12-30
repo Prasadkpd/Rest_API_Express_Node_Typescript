f849c8dfa0545f3473782a4439abf87d61663245
Prasad Lakshan <kpdplakshan@gmail.com>
2022-12-30 19:49:11 +0530

    changes

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
-import express from 'express';
-import config from 'config';
-import log from './logger';
+import express from "express";
+import config from "config";
+import log from "./logger";
 import connect from "./db/connect";
 import routes from "./routes";
 
@@ -10,11 +10,11 @@ const host = config.get("host") as string;
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 
 app.listen(port, host, () => {
-    log.info(`Server is listing at http://${host}:${port}`);
+  log.info(`Server is listing at http://${host}:${port}`);
 
-    connect().then();
-    routes(app);
-})
+  connect().then();
+  routes(app);
+});
